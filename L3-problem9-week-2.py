aa61b2212b32c39876d753034e37eaf03ae85564
Alan García Mendoza <alangm7@outlook.com>
2016-06-16 09:52:49 -0500

    Update L3-problem9-week-2.py

--- L3-problem9-week-2.py
+++ L3-problem9-week-2.py
@@ -1,13 +1,27 @@
-"""The program works as follows: you (the user) thinks of an integer between 0 (inclusive) and 100 (not inclusive). The computer makes guesses, and you give it input - is its guess too high or too low? Using bisection search, the computer will guess the user's secret number!
-"""
+print("Please think of a number between 0 and 100!")
 
-from random import randint
+# At the start the highest the number could be is 100 and the lowest is 0.
+hi = 100
+lo = 0
+guessed = False
 
-number = input('Please think w number between 0 and 100: ')
+# Loop until we guess it correctly
+while not guessed:
+    # Bisection search: guess the midpoint between our current high and low guesses
+    guess = (hi + lo)/2
+    print("Is your secret number " + str(guess)+ "?")
+    user_inp = raw_input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. ")
 
-random = print ('Is your secret number ' + str(randint(0,100)) + '?')
+    if user_inp == 'c':
+        # We got it right!
+        guessed = True
+    elif user_inp == 'h':
+        # Guess was too high. So make the current guess the highest possible guess.
+        hi = guess
+    elif user_inp == 'l':
+        # Guess was too low. So make the current guess the lowest possible guess.
+        lo = guess
+    else:
+        print("Sorry, I did not understand your input.")
 
-letter = input ("Enter 'h' to indicate the guess is too hight. Enter 'l' to indicate the guess is low. Enter 'c' to indicate I guessed correctly.) + str(number)")
-
-if letter == 'h':
-    print ('Is your secret number ' + str(randint(0,number)) + '?')
+print('Game over. Your secret number was: ' + str(guess))
